133cd4d31130cdced146606e2073e461bfddabe9
jonas <jonassim23@gmail.com>
2026-05-29 16:01:03 +0800

    chore:修改启动参数

--- document/deploy/server-prod.sh
+++ document/deploy/server-prod.sh
@@ -2,16 +2,10 @@
 
 # ================= 配置 =================
 ENV="prod"
-REPO_PATH="/home/jia/workspace/multi/multi-${ENV}/multi-server"
+REPO_PATH="/home/jia/workspace/java-template"
 BRANCH_NAME="main"
-MODULE=$1
-IMAGE="${MODULE}:${ENV}"
-
-# ================= 校验 =================
-if [[ -z "$MODULE" ]]; then
-  echo "❌ 请指定模块名: (service-client / service-admin)"
-  exit 1
-fi
+SERVICE="java-template"
+IMAGE="${SERVICE}:${ENV}"
 
 # ================= 执行 =================
 echo "进入仓库目录: $REPO_PATH"
@@ -29,7 +23,7 @@ git reset --hard origin/$BRANCH_NAME || { echo "❌ Git 拉取失败"; exit 1; }
 echo "清除旧镜像: $IMAGE ..."
 docker rmi -f "$IMAGE" 2>/dev/null
 
-echo "构建并启动服务: $MODULE ..."
-docker compose -f docker-compose.yml -f docker-compose.${ENV}.yml up -d --build "$MODULE" || { echo "❌ Docker Compose 启动失败"; exit 1; }
+echo "构建并启动服务: $SERVICE ..."
+docker compose -f docker-compose.yml -f docker-compose.${ENV}.yml up -d --build "$SERVICE" || { echo "❌ Docker Compose 启动失败"; exit 1; }
 
-echo "✅ 部署完成: $MODULE"
+echo "✅ 部署完成: $SERVICE"
