cd62dc8e58f406d173c0797bb2d9cb8565ebf9e0
jonas <jonassim23@gmail.com>
2026-06-16 16:27:37 +0800

    feat:SQL优化

--- document/sql/versioned/ddl.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `auth_perm`
-    ADD COLUMN `component` varchar(100) DEFAULT NULL COMMENT '前端组件路径' AFTER `path`;
--- document/sql/versioned/dml.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `auth_perm` SET `component` = 'system/UserManageView' WHERE `code` = 'auth:user';
-UPDATE `auth_perm` SET `component` = 'system/RoleManageView' WHERE `code` = 'auth:role';
-UPDATE `auth_perm` SET `component` = 'system/MenuManageView' WHERE `code` = 'auth:perm';
